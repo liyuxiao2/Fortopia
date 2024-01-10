91753b4fdeb4e8367b73ddbac3d4971d01b9bc70
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2024-01-10 14:07:47 -0500

    added 2 superclasses runner/obstacles

---
+++ Fortopia/Runner.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Runner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Runner extends SuperSmoothMover
+{
+    /**
+     * Act - do whatever the Runner wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
