3541879ba2f9b7cbac8ad8aede3d5b891a647d30
natalieh2 <150183401+natalieh2@users.noreply.github.com>
2024-01-12 13:50:53 -0500

    renamed scrollign wrold methods

--- Fortopia/MyWorld.java
+++ Fortopia/MyWorld.java
@@ -3,10 +3,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
+ * @author Natalie
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class MyWorld extends ScrollingWorld
 {
 
     /**
@@ -14,8 +14,7 @@ public class MyWorld extends World
      * 
      */
     public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1024, 700, 1); 
+    {
+        super(1024, 700, 1);
     }
 }
